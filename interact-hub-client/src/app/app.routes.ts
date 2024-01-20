bfb0a0793bc90e5f5d3702277671ec3fd3a41aec
Mihajlo Madic <mihajlo.madic@elfak.rs>
2024-01-20 13:49:02 +0100

    routes

--- interact-hub-client/src/app/app.routes.ts
+++ interact-hub-client/src/app/app.routes.ts
@@ -4,8 +4,7 @@ import { CreatePostComponent } from './create-post/create-post.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 
 export const routes: Routes = [
-    { path: 'login', pathMatch: 'full', component: LoginComponentComponent },
-    { path: 'signUp', pathMatch: 'full', component: LoginComponentComponent },
-    { path: '', pathMatch: 'full', component: FrontPageComponent },
-
+  { path: 'login', pathMatch: 'full', component: LoginComponentComponent },
+  { path: 'signUp', pathMatch: 'full', component: LoginComponentComponent },
+  { path: '', redirectTo: '/signUp', component: FrontPageComponent },
 ];
